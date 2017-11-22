43b6e8ad3fdc4c2fc930e5cc43cc5ab4cf4c1ce9
skoneczna.malwina@gmail.com <skoneczna.malwina@gmail.com>
2017-11-22 18:10:11 +0000

    Initial commit

---
+++ src/main/java/com/malwinas/employee/EmployeeApplication.java
@@ -0,0 +1,19 @@
+package com.malwinas.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EmployeeApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeApplication.class);
+	}
+}
